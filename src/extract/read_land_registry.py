040e566e90d7b08b8a5ddaedd4e8fecb20e6b00d
Nmudassar <nadia.syed77@gmail.com>
2026-08-13 16:52:16 +0100

    feat: add Land Registry extraction and create data transformation structure

--- src/extract/read_land_registry.py
+++ src/extract/read_land_registry.py
@@ -115,4 +115,75 @@ df["tenure"] = df["tenure"].map(tenure_mapping)
 print("\nTenure values after transformation:")
 print(df["tenure"].value_counts())
 
-# step 18: Create a full address from the separate address columns
+# step 18: Create a full address from the separate address columns.For every property row, take the address fields, remove missing values, convert the remaining values to text, join them with commas, and save the result in a new column called full_address.
+address_columns = [
+    "paon",
+    "saon",
+    "street",
+    "locality",
+    "town_city",
+    "postcode",
+]
+# Create full address and ignore missing address parts
+df["full_address"] = df[address_columns].apply(
+    lambda row: ", ".join(row.dropna().astype(str)),
+    axis=1
+)
+
+
+# Show the first 10 full addresses
+print("\nSample full addresses:")
+print(df["full_address"].head(10))
+
+# step 19: Clean the postcode
+df["postcode"] = (
+    df ["postcode"]
+    .str.strip() # remove leading and trailing whitespace
+    .str.upper() # convert to uppercase
+)
+
+# Show sample postcodes after cleaning
+print("\nCleaned postcodes:")
+print(df["postcode"].head(10))
+
+
+# Count properties with missing postcodes
+print("\nMissing postcodes:")
+print(df["postcode"].isnull().sum())
+
+# step 20: Count the duplicate transcations ID
+print("\nDuplicate transaction IDs:")
+print(df["transaction_id"].duplicated().sum())
+
+# step 21: Because postcode will be important later when joining with EPC and ONS data, we should understand those 296 missing records before deciding what to do with them.
+# 21a: Find rows where postcode is missing
+
+missing_postcodes = df [df["postcode"].isnull()]
+
+print("\nRows with the missing postcodes:")
+print(missing_postcodes.shape[0])
+
+print("\nSample record with missing postcodes")
+print(
+    missing_postcodes[
+        [
+            "transaction_id",
+            "paon",
+            "saon",
+            "street",
+            "locality",
+            "town_city",
+            "county",
+            "postcode"
+        ]
+    ].head(10)
+)
+    #A better Data Engineering approach is to keep them and flag them.
+
+# Step  — Create a postcode quality flag
+
+# Create a flag whter property have postcode or not 
+df["has_postcode"] = df["postcode"].notnull()
+
+print('\nPostcode availability flag:')
+print(df["has_postcode"].value_counts())
